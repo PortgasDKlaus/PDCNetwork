8e5c8271d8b5b4a663b3ac8efda26106349835d7
PortgasDCrow <2961494530@qq.com>
2015-09-06 10:49:33 +0800

    Start
    Start a Proj

---
+++ PDCNetwork/PDCNetwork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDCNetwork
+//
+//  Created by portgasdcrow on 15/9/6.
+//  Copyright (c) 2015年 PDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
